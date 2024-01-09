56e2aa88c9dbb48c15570f3f001e646448773349
noway <daviddavide847@gmail.com>
2024-01-09 20:40:55 +0000

    ff

--- main.cpp
+++ main.cpp
@@ -1,18 +1,27 @@
+
+/********************************************************************************************************************
+ * 
+Alcune librerie e funzioni potrebbero non essere disponibili su tutti i sistemi operativi.
+Per problemi di compatibilità su linux e altri IDE, la funzione sleep() è stata commentata.
+
+********************************************************************************************************************/
+
 #include <iostream>
 #include <stdlib.h>
 //#include <Windows.h>
-//#include <system>
 #include<unistd.h>   
 #include <cstdlib>
 #include <bits/stdc++.h>
 #include <algorithm>
 
 using namespace std;
+const int MAX_ARRAY = 100;
 
 struct vocabolario {
     string ita;
     string eng;
 };
+
 
 void aggiungiVocabolo(int nparola, vocabolario arrayDizionario[]) {
     cout << "inserire vocabolo in italiano: ";
@@ -21,7 +30,7 @@ void aggiungiVocabolo(int nparola, vocabolario arrayDizionario[]) {
     cin >> arrayDizionario[nparola].eng;
     cout << "Vocabolo aggiunto con successo!\n" << arrayDizionario[nparola].ita << " - " << arrayDizionario[nparola].eng << endl;
     //sleep(3000);
-    //system("clear");
+    system("clear");
     return;
 }
 
@@ -138,13 +147,12 @@ void exportDizionario(vocabolario arrayDizionario[],int nparola) {
     ofstream DizionarioTXT;
     DizionarioTXT.open("dizionario.txt", ios::out);
 
+    //cercare parole dentro al file.txt e importarle nel dizionario
     for (int i = 0; i < nparola; i++) {
         DizionarioTXT << arrayDizionario[i].ita << "-" << arrayDizionario[i].eng << endl;
-        cout << arrayDizionario[i].ita << "-" << arrayDizionario[i].eng << endl;
     }
-    cout << "Dizionario esportato con successo." << endl;
+    cout << "Dizionario esportato con successo.\n" << endl;
     DizionarioTXT.close();
-
     return;
 }
 
@@ -152,11 +160,13 @@ void importDizionario(vocabolario arrayDizionario[], int& nparola) {
     ifstream DizionarioTXT;
     DizionarioTXT.open("dizionario.txt", ios::in);
 
+    //caso file non esistente
     if (!DizionarioTXT) {
         cerr << "Impossibile trovare il file dizionario.txt";
         return;
     }
 
+    //cercare parole dentro al file.txt e importarle nel dizionario
     string line;
     while (getline(DizionarioTXT, line)) {
         size_t pos = line.find('-');
@@ -170,55 +180,133 @@ void importDizionario(vocabolario arrayDizionario[], int& nparola) {
     DizionarioTXT.close();
 }
 
+// void visualizzaMenu(int nparola,string& parola,string& searchWord,string& parolaDaSostituire,string& temp,vocabolario arrayDizionario[]) {
+    
+
+        // //add vocaboli
+        // if (selezione == 1) {
+        //     aggiungiVocabolo(nparola, arrayDizionario);
+        //     nparola++;
+        // }
+        // //view vocaboli
+        // else if (selezione == 2) {
+        //     //sleep(3000);
+        //     visualizzaDizionario(nparola, arrayDizionario);
+        // }
+        // //del/modify vocaboli
+        // else if (selezione == 3) {
+        //     editDizionario(nparola, selezione, searchWord, parolaDaSostituire, arrayDizionario);
+        // }
+        // //bubble sort vocaboli
+        // else if (selezione == 4) {
+        //     ordinamentoDizionario(temp, selezione, nparola, arrayDizionario);
+        // }
+        // //esporta dizionario file txt
+        // else if (selezione == 5) {
+        //     exportDizionario(arrayDizionario, nparola);
+        // }
+        // //importa dizionario file txt
+        // else if (selezione == 6) {
+        //     importDizionario(arrayDizionario, nparola);
+        // }
+        // else if (selezione == 7) {
+        //     break;
+        // }        
+        // //nessuno dei casi
+        // else {
+        //     break;
+        // }
+// }
+
 int main() {
-    const int MAX_ARRAY = 100;
+    //const int MAX_ARRAY = 100;
     int selezione, nparola = 0;
     string parola, searchWord, parolaDaSostituire, temp;
     vocabolario arrayDizionario[MAX_ARRAY];
     //menų
+    // visualizzaMenu(nparola, parola, searchWord, parolaDaSostituire, temp, arrayDizionario);
     do {
-        cout << "1. Aggiungere vocaboli al dizionario\n";
-        cout << "2. Visualizzare dizionario\n";
-        cout << "3. Modificare o cancellare vocaboli\n";
-        cout << "4. Ordinare dizionario\n";
-        cout << "5. Esporta dizionario in dizionario.txt\n";
-        cout << "6. Importa dizionario da dizionario.txt\n";
-        cout << "7. Esci\n";
-        cout << "selezione: ";
-        cin >> selezione;
-        //add vocaboli
-        if (selezione == 1) {
+    cout << R"(
+1. Aggiungere vocaboli al dizionario
+2. Visualizzare dizionario
+3. Modificare o cancellare vocaboli
+4. Ordinare dizionario
+5. Esporta dizionario in dizionario.txt
+6. Importa dizionario da dizionario.txt
+0. Esci
+    )";
+    cout << "selezione: ";
+    cin >> selezione;
+    switch(selezione) {
+        case 1:
             aggiungiVocabolo(nparola, arrayDizionario);
             nparola++;
-        }
-        //view vocaboli
-        else if (selezione == 2) {
-            //sleep(3000);
+            break;
+        case 2:
             visualizzaDizionario(nparola, arrayDizionario);
-        }
-        //del/modify vocaboli
-        else if (selezione == 3) {
+            break;
+        case 3:
             editDizionario(nparola, selezione, searchWord, parolaDaSostituire, arrayDizionario);
-        }
-        //bubble sort vocaboli
-        else if (selezione == 4) {
+            break;
+        case 4:
             ordinamentoDizionario(temp, selezione, nparola, arrayDizionario);
-        }
-        //esporta dizionario file txt
-        else if (selezione == 5) {
+            break;
+        case 5:
             exportDizionario(arrayDizionario, nparola);
-        }
-        //importa dizionario file txt
-        else if (selezione == 6) {
+            break;
+        case 6:
             importDizionario(arrayDizionario, nparola);
-        }
-        else if (selezione == 7) {
             break;
-        }        
-        //nessuno dei casi
-        else {
+        case 7:
             break;
-        }
-    } while (true);
+        default:
+            break;
+    }
+} while (selezione);
+
+    // do {
+    //     cout << "1. Aggiungere vocaboli al dizionario\n";
+    //     cout << "2. Visualizzare dizionario\n";
+    //     cout << "3. Modificare o cancellare vocaboli\n";
+    //     cout << "4. Ordinare dizionario\n";
+    //     cout << "5. Esporta dizionario in dizionario.txt\n";
+    //     cout << "6. Importa dizionario da dizionario.txt\n";
+    //     cout << "7. Esci\n";
+    //     cout << "selezione: ";
+    //     cin >> selezione;
+    //     //add vocaboli
+    //     if (selezione == 1) {
+    //         aggiungiVocabolo(nparola, arrayDizionario);
+    //         nparola++;
+    //     }
+    //     //view vocaboli
+    //     else if (selezione == 2) {
+    //         //sleep(3000);
+    //         visualizzaDizionario(nparola, arrayDizionario);
+    //     }
+    //     //del/modify vocaboli
+    //     else if (selezione == 3) {
+    //         editDizionario(nparola, selezione, searchWord, parolaDaSostituire, arrayDizionario);
+    //     }
+    //     //bubble sort vocaboli
+    //     else if (selezione == 4) {
+    //         ordinamentoDizionario(temp, selezione, nparola, arrayDizionario);
+    //     }
+    //     //esporta dizionario file txt
+    //     else if (selezione == 5) {
+    //         exportDizionario(arrayDizionario, nparola);
+    //     }
+    //     //importa dizionario file txt
+    //     else if (selezione == 6) {
+    //         importDizionario(arrayDizionario, nparola);
+    //     }
+    //     else if (selezione == 7) {
+    //         break;
+    //     }        
+    //     //nessuno dei casi
+    //     else {
+    //         break;
+    //     }
+    // } while (true);
     return 0;
 }
